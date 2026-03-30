c436d3bc98bea1cc76b5391b6341ce5524123a2f
Wittawat <wittawat.ict@gmail.com>
2026-03-30 08:31:26 +0700

    Update master.routes.js

--- routes/master.routes.js
+++ routes/master.routes.js
@@ -4,8 +4,9 @@ const masterController = require("../controller/master.controller");
 const verifyToken = require('../middleware/verifyToken');
 
 // Plant Master
-router.post("/setplant", verifyToken, masterController.setPlant);
-router.post("/saveplant", verifyToken, masterController.savePlant);
-
+//router.post("/setplant", verifyToken, masterController.setPlant);
+//router.post("/saveplant", verifyToken, masterController.savePlant);
+router.post("/setplant", masterController.setPlant);
+router.post("/saveplant", masterController.savePlant);
 
 module.exports = router;
