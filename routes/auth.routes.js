e126d8a4f16d3f4a04d205d2d646a16d42ed1330
Wittawat <wittawat.ict@gmail.com>
2026-03-31 10:31:19 +0700

    update auth

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -8,9 +8,23 @@ router.post('/login', authController.login);
 // refresh token
 router.post('/refresh', authController.refresh);
 
-// Menu Master
+// Menu Auth
 router.post("/setmenu", authController.setMenu);
 router.post("/savemenu", authController.saveMenu);
 router.post("/deletemenu", authController.deleteMenu);
 
+// Submenu Auth
+router.post("/setsubmenu", authController.setSubmenu);
+router.post("/savesubmenu", authController.saveSubmenu);
+router.post("/deletesubmenu", authController.deleteSubmenu);
+
+// Permission Auth
+router.post("/setpermission", authController.setPermission);
+router.post("/savepermission", authController.savePermission);
+router.post("/deletepermission", authController.deletePermission);
+
+// Account Auth
+router.post("/setaccount", authController.setAccount);
+router.post("/saveaccount", authController.saveAccount);
+router.post("/deleteaccount", authController.deleteAccount);
 module.exports = router;
