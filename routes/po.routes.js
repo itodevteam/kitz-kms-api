5728e5323b84d6e48d321d60e115468666358ed9
Wittawat <wittawat.ict@gmail.com>
2026-04-01 09:33:35 +0700

    uodate

--- routes/po.routes.js
+++ routes/po.routes.js
@@ -4,10 +4,10 @@ const poController = require("../controller/po.controller");
 const verifyToken = require('../middleware/verifyToken');
 
 // PO Orders
-router.post("/upload", verifyToken, poController.uploadPO);
-router.get("/master", verifyToken, poController.getPOMaster);
-router.post("/waitprepare", verifyToken, poController.getPOWaitPrepare);
-router.post("/waitapprove", verifyToken, poController.getPOWaitApprove);
-router.post("/approval", verifyToken, poController.poApproval);
+router.post("/upload", poController.uploadPO);
+router.get("/master", poController.getPOMaster);
+router.post("/waitprepare", poController.getPOWaitPrepare);
+router.post("/waitapprove", poController.getPOWaitApprove);
+router.post("/approval", poController.poApproval);
 
 module.exports = router;
