b40a702fc5d05aa02a0a3c749da99b29746ad540
TEAM-NB88\Administrator <ito.ptt@hotmail.com>
2026-04-01 08:53:13 +0700

    clear temp files

--- routes/dashboard.routes.js
+++ routes/dashboard.routes.js
@@ -5,8 +5,7 @@ const dashboardController = require("../controller/dashboard.controller");
 module.exports = function (io) {
   const router = express.Router();
 
-  router.post(
-    "/dashboard/waitingdata",
+  router.post("/dashboard/waitingdata",
     verifyToken,
     dashboardController.getWaitingData(io)
   );
