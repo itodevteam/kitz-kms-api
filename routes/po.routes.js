be03bbeb715d7f2ecec8ac038604623977c68c65
Wittawat <wittawat.ict@gmail.com>
2026-04-01 08:08:28 +0700

    Update route

--- routes/po.routes.js
+++ routes/po.routes.js
@@ -4,12 +4,8 @@ const poController = require("../controller/po.controller");
 const verifyToken = require('../middleware/verifyToken');
 
 // PO Orders
-<<<<<<< HEAD
-router.post("/upload", poController.uploadPO);
-=======
 router.post("/upload", verifyToken, poController.uploadPO);
 router.get("/master", verifyToken, poController.getPOMaster);
->>>>>>> 1164fe9fec4c8ecfd2898e646c7a7eb6b7b80187
 router.post("/waitprepare", verifyToken, poController.getPOWaitPrepare);
 router.post("/waitapprove", verifyToken, poController.getPOWaitApprove);
 router.post("/approval", verifyToken, poController.poApproval);
