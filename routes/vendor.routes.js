62009a45c0e409323479105e89a6ad4b380408a4
TEAM-NB88\Administrator <ito.ptt@hotmail.com>
2026-04-08 16:33:09 +0700

    Update vender name

--- routes/vendor.routes.js
+++ routes/vendor.routes.js
@@ -4,7 +4,7 @@ const vendorController = require("../controller/vendor.controller");
 const verifyToken = require('../middleware/verifyToken');
 const { route } = require("./po.routes");
 
-router.post("/getVendorWaitConfirm", vendorController.getVendorWaitConfirm);
-router.post("/poVendorConfirm", vendorController.poVendorConfirm);
+router.post("/waitconfirm", vendorController.getVendorWaitConfirm);
+router.post("/confirm", vendorController.poVendorConfirm);
 
 module.exports = router;
