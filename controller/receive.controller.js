5b0215fd34488b13e1d0de9ebf55dd5f535a99d8
TEAM-NB88\Administrator <ito.ptt@hotmail.com>
2026-04-24 16:04:45 +0700

    update receive

--- controller/receive.controller.js
+++ controller/receive.controller.js
@@ -1,11 +1,11 @@
 const e = require("cors");
-const qcServices = require("../services/receive.services");
+const receiveService = require("../services/receive.services");
 
 exports.getDeliveryMaster = async (req, res) => {
   try {
     const data = req.body;  
 
-    const result = await qcServices.getDeliveryMaster(data);
+    const result = await receiveService.getDeliveryMaster(data);
 
     res.json({
       success: result.info?.[0]?.success === 1,
@@ -22,7 +22,7 @@ exports.getDeliveryDetail = async (req, res) => {
   try {
     const data = req.body;
 
-    const result = await qcServices.getDeliveryDetail(data);
+    const result = await receiveService.getDeliveryDetail(data);
 
     res.json({
       success: result.info?.[0]?.success === 1,
@@ -39,7 +39,7 @@ exports.getReceiveDetail = async (req, res) => {
   try {
     const data = req.body;
 
-    const result = await qcServices.getReceiveDetail(data);
+    const result = await receiveService.getReceiveDetail(data);
 
     res.json({
       success: result.info?.[0]?.success === 1,
@@ -59,7 +59,7 @@ exports.getReceiveDetail = async (req, res) => {
 exports.confirmReceive = async (req, res) => {
   try {
     const data = req.body;
-    const result = await qcServices.confirmReceive(data);
+    const result = await receiveService.confirmReceive(data);
 
     res.json({
       success: result.info?.[0]?.success === 1,
--- routes/receive.routes.js
+++ routes/receive.routes.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const qcController = require("../controller/receive.controller");
+const receiveController = require("../controller/receive.controller");
 const verifyToken = require('../middleware/verifyToken');
 
-router.post("/master", verifyToken, qcController.getDeliveryMaster);
-router.post("/detail", verifyToken, qcController.getDeliveryDetail);
-router.post("/receive", verifyToken, qcController.getReceiveDetail);
-router.post("/confirm", verifyToken, qcController.confirmReceive);
+router.post("/master", receiveController.getDeliveryMaster);
+router.post("/detail", receiveController.getDeliveryDetail);
+router.post("/receive", receiveController.getReceiveDetail);
+router.post("/confirm", receiveController.confirmReceive);
 
 module.exports = router;
 
